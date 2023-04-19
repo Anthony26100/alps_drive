d7e9a490efd27fd583f5979711bef53618059667
Anthony <anthony.bellier@laposte.net>
2023-04-19 22:35:57 +0200

    test maison promise

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
-// app.get("/", (req, res) => {
-//   res.send("Hello Worlds!");
-// });
+const fs = require("fs");
+const os = require("os");
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
 app.use(express.static("frontend"));
+
+app.get("/api/drive", (req, res) => {
+  const temp = os.tmpdir();
+
+  const folder = fs.promises.readdir(temp, { withFileTypes: true });
+
+  folder.then((e) => {
+    e.map((data) => {
+      if (data.isDirectory()) {
+        return {
+          name: data.name,
+          isFolder: data.isDirectory(),
+        };
+      } else {
+        return data;
+      }
+    });
+  });
+  console.log("error :");
+  res.send(folder);
+});
